dc84d804f63aeaadbbefd152c9e35d81dabb9b0b
Carlos Bueno <carlos.bueno@sfonline.pt>
2018-05-18 23:00:34 +0100

    Teste

--- config/env/production.js
+++ config/env/production.js
@@ -374,7 +374,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://regoldi.herokuapp.com/',
+    baseUrl: 'https://regoldi.herokuapp.com',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
