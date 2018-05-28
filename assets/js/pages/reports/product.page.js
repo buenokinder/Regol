d369d7b2ad7ea2b104997712e6100538ee653b2f
Carlos <carlos.bueno@sfponline.pt>
2018-05-28 10:20:51 +0100

    Final

--- assets/js/pages/reports/product.page.js
+++ assets/js/pages/reports/product.page.js
@@ -1,4 +1,4 @@
-parasails.registerPage('product', {
+parasails.registerPage('productReport', {
     //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
     //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
     //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
